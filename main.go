c2cee84972f4aa804bc6b719a4d2b49229311592
João Félix <jafelix.22@gmail.com>
2022-12-28 13:50:59 +0000

    env file added

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":3000")
+	r.Run(fmt.Sprint(":", port))
 }
